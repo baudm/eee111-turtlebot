bca0b77e5b27bd7b789869027588d65220e99489
Darwin Bautista <daruuin@gmail.com>
2019-09-26 18:24:09 +0800

    Automate raspi-config and network configuration

--- README.md
+++ README.md
@@ -1,11 +1,18 @@
 # eee111-turtlebot
 Turtlebot Setup
 
-## raspi-config
-1. `Localization Options` -> `Change Timezone` -> `Asia`. Select `Manila`.
-2. `Boot Options` -> `Desktop/CLI`. Select `Console`.
-3. `Boot Options` -> `Wait for Network at Boot`. Select `Yes`.
+## Network Configuration
+All EEE 111 devices should have a static IP and should use the subnet 10.158.25.0/24:
+- TurtleBots should use .10 to .99
+- Workstations should use .100 to .199
 
-## Turtlebot Bringup
-1. Put the correct WiFi SSID and pre-shared key into `wpa_supplicant.conf`.
-2. Run the setup script as root: `sudo ./setup.sh`
+## Automated Setup
+The setup.sh script will need the following details:
+1. WiFi SSID
+2. WiFi PSK (password)
+3. Last octet of the IP address
+
+Run the script as root, e.g.:
+```
+./setup.sh EEE-WIFI password 17
+```
--- setup.sh
+++ setup.sh
@@ -1,5 +1,55 @@
 #!/bin/sh
 
-install -D -m755 turtlebot-bringup.sh /usr/local/bin/turtlebot-bringup.sh
-install -D -m644 turtlebot-bringup.service /lib/systemd/system/turtlebot-bringup.service
-install -D -m644 wpa_supplicant.conf /etc/wpa_supplicant/wpa_supplicant.conf
+if [ ! "$#" -eq 3 ]; then
+    echo "Usage: $0 <SSID> <PSK> <last octet of IP address>"
+    exit 1
+fi
+
+# For testing:
+PREFIX=
+
+SSID="$1"
+PSK="$2"
+IP="10.158.25.${3}"
+
+cat<<EOF
+Using the following configuration:
+
+SSID = $SSID
+PSK = $PSK
+IP = $IP
+EOF
+
+# Set correct timezone
+raspi-config nonint do_change_timezone Asia/Manila
+
+# Boot to text console, requiring user to login
+raspi-config nonint do_boot_behaviour B1
+
+# TurtleBot "bringup" nodes
+install -D -m755 turtlebot-bringup.sh "$PREFIX/usr/local/bin/turtlebot-bringup.sh"
+install -D -m644 turtlebot-bringup.service "$PREFIX/lib/systemd/system/turtlebot-bringup.service"
+
+# Network configuration (WiFi)
+mkdir -p "$PREFIX/etc/wpa_supplicant"
+cat >"$PREFIX/etc/wpa_supplicant/wpa_supplicant.conf" <<EOF
+ctrl_interface=DIR=/var/run/wpa_supplicant GROUP=netdev
+update_config=1
+country=PH
+
+network={
+        ssid="$SSID"
+        psk="$PSK"
+        key_mgmt=WPA-PSK
+}
+EOF
+
+# Network configuration (IP Address)
+mkdir -p "$PREFIX/etc"
+cat dhcpcd.conf.template - >"$PREFIX/etc/dhcpcd.conf" <<EOF
+interface wlan0
+static ip_address=${IP}/24
+static routers=10.158.25.1
+static domain_name_servers=10.32.1.7 10.16.3.143
+
+EOF
